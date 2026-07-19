5872d4fbc1dbf872e037e75e38810fb9ca9619b6
varakin <varakin_ivan@yahoo.com>
2026-07-20 01:28:42 +0300

    github action

--- conftest.py
+++ conftest.py
@@ -16,7 +16,7 @@ PASSWORD = os.getenv("PASSWORD")
 @pytest.fixture(scope="session")
 def driver():
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
