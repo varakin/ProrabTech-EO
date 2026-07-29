e027d14b2af9037d447589cbe02258bc863d000b
varakin <varakin_ivan@yahoo.com>
2026-07-29 22:44:35 +0300

    fix headless

--- conftest.py
+++ conftest.py
@@ -18,7 +18,7 @@ PASSWORD = os.getenv("PASSWORD")
 @pytest.fixture(scope="session")
 def driver():
     options = Options()
-    # options.add_argument("--headless") # включить для ci/cd
+    options.add_argument("--headless") # включить для ci/cd
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
