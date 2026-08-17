d97bdbee9af03658e48aa9c8e585ff2ed54d2e75
varakin <varakin_ivan@yahoo.com>
2026-08-18 00:47:37 +0300

    added marks

--- tests/test_object_home_page.py
+++ tests/test_object_home_page.py
@@ -11,12 +11,16 @@ def test_new_object(driver, authorization, dashboard, object_home_page):
     object_home_page.wait_loader()
     object_home_page.checking_description()
 
+@pytest.mark.regression
+@pytest.mark.smoke
 def test_element_of_page(driver, authorization, dashboard, object_home_page):
     authorization.auth_test10()
     object_home_page.enter_object_for_tests()
     object_home_page.wait_loader()
     object_home_page.check_name_menu()
 
+@pytest.mark.regression
+@pytest.mark.smoke
 def test_return_dashboard(driver, authorization, dashboard, object_home_page):
     authorization.auth_test10()
     object_home_page.enter_object_for_tests()
